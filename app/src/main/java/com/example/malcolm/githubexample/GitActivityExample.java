9fdd159a44b742918b1ddd6010f9d239e64050c1
malcolm_17 <malcolmjrobinson@gmail.com>
2017-10-08 18:57:06 +0100

    Initial commit

---
+++ app/src/main/java/com/example/malcolm/githubexample/GitActivityExample.java
@@ -0,0 +1,13 @@
+package com.example.malcolm.githubexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GitActivityExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git_example);
+    }
+}
